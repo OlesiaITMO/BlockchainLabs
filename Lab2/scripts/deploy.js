37b5e645c2409bedfc0c7ab199852d1619c97b4a
Olesia Safronova <kotputya@gmail.com>
2026-03-04 13:39:01 +0300

    Add Lab2: ERC20 token (Hardhat) + deploy scripts

---
+++ Lab2/scripts/deploy.js
@@ -0,0 +1,19 @@
+﻿const { ethers } = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+  const tokenName = process.env.TOKEN_NAME || "OlesyaToken";
+  const tokenSymbol = process.env.TOKEN_SYMBOL || "OLS";
+
+  const MyToken = await ethers.getContractFactory("MyToken");
+  const token = await MyToken.deploy(tokenName, tokenSymbol);
+
+  await token.waitForDeployment();
+
+  console.log("Deployed to:", await token.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
